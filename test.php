b54adeae0376dce0a211c56879aa0f5dc3ea7482
Frej Wikström <snuffe_99@hotmail.com>
2017-05-16 10:08:46 +0300

    Fixed php + webconnection

--- test.php
+++ test.php
@@ -6,15 +6,14 @@
 <body>
 
 <?php
-	header("Content-type: text/plain; charset=utf-8");
 
 	$host = 'localhost';
 
-	$dbName = 'sgu';
+	$dbName = 'SGU';
 
-	$username = 'root';
+	$username = 'sec_sgu';
 
-	$password = 'root';
+	$password = 'Abcd1234';
 
 
 	$dbCon = new PDO("mysql:host=".$host.";dbname=".$dbName, $username, $password);
@@ -34,25 +33,49 @@
     	echo  $row[2] . " | " . $row[3] ."-". $row[4] . " | " . $row[5] . " | " . $row[6] . "<br/>";
 
 	}
-
-	  
 ?>
+	  
 
-	<form>
-		<input type="text" name="fName">
-		<input type="text" name="lName">
-		<input type="tel" name="tel">
-		<input type="email" name="email">
-		<input type="submit" name="submit">	
+
+	<form action="index.php" method="post">
+
+		<p>
+			<label for="firstName">First name</label>
+			<input type="text" name="fName">
+		</p>
+
+		<p>
+			<label for="lastName"> Last name</label>
+			<input type="text" name="lName">
+		</p>
+
+		<p>
+			<label for="phoneNumber">Phone number</label>
+			<input type="tel" name="tel">
+		</p>
+
+		<p>
+			<label for="e-mail"> e-mail</label>
+			<input type="email" name="email">
+		</p>
+
+		<input type="submit" name="submit">
+
 	</form>
 
-<?php
-	
-	if ($_GET['fname'] != NULL){
-		$sql = "INSERT INTO customer (fname, lname, tel, e-mail)
+
+<?php	
+	if ($_GET['fName'] != NULL){
+		$sql = "INSERT INTO customer (fName, lName, tel, email)
 		VALUES('$_GET'['fName'], '$_GET'['lName'], '$_GET'['tel'], '$_GET'['email'])";
 	}
- ?>
+?>
+
+<?php
+	$sql = "INSERT INTO customer (fName, lName, tel, email) VALUES
+		('Cedrik','Svedblod','0324453972','cedrik.svedblod@gmail.com')"
+
+?>
 
  
 </body>
